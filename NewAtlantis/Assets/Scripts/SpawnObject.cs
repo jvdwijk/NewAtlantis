68214c113058d224f80220e1b9f4e80d68cecd2a
jvdwijk <unconfigured@null.spigotmc.org>
2017-02-13 23:41:29 +0100

    spawn object script

---
+++ NewAtlantis/Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour {
+
+	public void Instantiate(GameObject item)
+    {
+        Instantiate(item,transform.position, Quaternion.identity);
+    }
+}
