deff2c499d9d36c5a2a789b39d88190b39cd87bc
Rico Evers <ricoevers@live.nl>
2017-02-15 16:30:55 +0100

    input manager 0.1

--- NewAtlantis/Assets/Scripts/InputManager.cs
+++ NewAtlantis/Assets/Scripts/InputManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour {
 
-    private float moveHorizontal;
+    /*private float moveHorizontal;
     private float moveVertical;
     private bool mouseClick;
 
@@ -12,6 +12,7 @@ public class InputManager : MonoBehaviour {
     {
         moveHorizontal = Input.GetAxis("Horizontal");
         moveVertical = Input.GetAxis("Vertical");
+        mouseClick = Input.GetButton("MouseClick");
     }    
 
     public float GetSetVertical
@@ -24,5 +25,25 @@ public class InputManager : MonoBehaviour {
     {
         get { return moveHorizontal; }
         set { moveHorizontal = value; }
+    }
+
+    public bool GetSetMouseClick
+    {
+        get { return mouseClick; }
+        set { mouseClick = value; }
+    }*/
+
+    public static InputManager inputManager;
+
+    public KeyCode[] buttons;
+    public string[] buttonNames;
+    public string[] buttonKeys;
+
+    void Awake()
+    {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i] = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(buttonNames[i], buttonKeys[i]));
+        }
     }
 }
