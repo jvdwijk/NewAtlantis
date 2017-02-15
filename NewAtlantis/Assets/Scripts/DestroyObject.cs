ca5934db076efab146d9b4a61ce027bc081a270f
jvdwijk <unconfigured@null.spigotmc.org>
2017-02-15 15:34:51 +0100

    DestroyObject

---
+++ NewAtlantis/Assets/Scripts/DestroyObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour {
+
+    [SerializeField]
+    private ObjectToMousePosition selectedTarget;
+
+
+    /// <summary>
+    /// function to cancel building
+    /// </summary>
+	public void Cancel()
+    {
+        print(selectedTarget.GetSetTargetObject);
+        //Destroy(selectedTarget.GetSetTargetObject);
+    }
+
+    /// <summary>
+    /// function to select object with raycasts 
+    /// </summary>
+    public void Demolish()
+    {
+
+    }
+
+    /// <summary>
+    /// Destroy selected object
+    /// </summary>
+    private void Destroy(GameObject target)
+    {
+        DestroyObject(target);
+    }
+}
