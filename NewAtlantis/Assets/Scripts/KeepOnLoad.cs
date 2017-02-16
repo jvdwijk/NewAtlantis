ce9456c1f9b79766d6fdd18fcb64c69a799ddabd
jvdwijk <unconfigured@null.spigotmc.org>
2017-02-16 15:00:11 +0100

    prefabs added & go to sleep, keep on load

---
+++ NewAtlantis/Assets/Scripts/KeepOnLoad.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepOnLoad : MonoBehaviour {
+
+    private KeepOnLoad thisScript;
+
+    void Awake()
+    {
+        if (thisScript == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            thisScript = this;
+        }
+        else if (thisScript != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- NewAtlantis/Assets/Scripts/MousePosition.cs
+++ NewAtlantis/Assets/Scripts/MousePosition.cs
@@ -7,19 +7,6 @@ public class MousePosition : MonoBehaviour {
     public static MousePosition position;
     public Vector3 mousePosition;
 
-    void Awake ()
-    {
-        if (position == null)
-        {
-            DontDestroyOnLoad(gameObject);
-            position = this;
-        }
-        else if (position != this)
-        {
-            Destroy(gameObject);
-        }
-    }
-
 	void FixedUpdate ()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
