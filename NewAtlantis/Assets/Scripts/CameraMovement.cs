5552b57d5d6c9503836cf2f1764f704e49bcf75c
jvdwijk <unconfigured@null.spigotmc.org>
2017-02-17 19:34:14 +0100

    Camera Movement

---
+++ NewAtlantis/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	void FixedUpdate () {
+		if(InputManager.input.IsKeyOn(InputManager.keyActions.left))
+        {
+            MoveCam(new Vector2(-1,0));
+        }
+        else if (InputManager.input.IsKeyOn(InputManager.keyActions.right))
+        {
+            MoveCam(new Vector2(1, 0));
+        }
+        if (InputManager.input.IsKeyOn(InputManager.keyActions.forward))
+        {
+            MoveCam(new Vector2(0, 1));
+        }
+        else if (InputManager.input.IsKeyOn(InputManager.keyActions.backward))
+        {
+            MoveCam(new Vector2(0,-1));
+        }
+    }
+
+    private void MoveCam(Vector2 direction)
+    {
+        transform.position += new Vector3(direction.x, 0, direction.y);
+    }
+}
--- NewAtlantis/Assets/Scripts/GoToSleep.cs
+++ NewAtlantis/Assets/Scripts/GoToSleep.cs
@@ -8,11 +8,17 @@ public class GoToSleep : MonoBehaviour {
     private MenuScript nightUI;
     [SerializeField]
     private Canvas selectedUI;
+    private float distance;
+    [SerializeField]
+    private GameObject player;
 
 	void Update () {
-		if(Input.GetKeyUp(InputManager.input.buttonMap[(int)InputManager.keyActions.action].buttonKey))
+
+        distance = Vector3.Distance(player.transform.position , transform.position);
+
+        if (Input.GetKeyUp(InputManager.input.buttonMap[(int)InputManager.keyActions.action].buttonKey) && distance <= 3)
         {
             nightUI.ScreenUpdate(selectedUI);
         }
-	}
+    }
 }
