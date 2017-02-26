4a3d08a64c1a427603fc65fa348e68331f178d32
jvdwijk <unconfigured@null.spigotmc.org>
2017-02-26 17:13:39 +0100

    start Saving and Loading

---
+++ NewAtlantis/Assets/Scripts/Serialization/Game.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[System.Serializable]
+public class Game {
+
+    public static Game current;
+    public Base mainBase;
+
+	public Game ()
+    {
+        mainBase = new Base();
+    }
+}
---
+++ NewAtlantis/Assets/Scripts/Serialization/SaveLoad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SaveLoad {
+
+    public static List<Game> savedGames = new List<Game>();
+
+    public void Save()
+    {
+        savedGames.Add(Game.current);
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/SavedGames.na");
+        binaryFormatter.Serialize(file,SaveLoad.savedGames);
+        file.Close();
+    }
+
+    public void Load()
+    {
+        if(File.Exists(Application.persistentDataPath + "/SavedGames.na"))
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/SavedGames.na", FileMode.Open);
+            SaveLoad.savedGames = (List<Game>)binaryFormatter.Deserialize(file);
+            file.Close();
+        }
+    }
+}
