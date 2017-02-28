2e4d8ad8a87109f422d722beae315b93bb34c165
jvdwijk <joeyvdwijk@gmail.com>
2017-02-28 15:13:27 +0100

    start load

---
+++ NewAtlantis/Assets/Scripts/Serialization/Load.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Load : MonoBehaviour {
+
+    [SerializeField]
+    private Canvas loadedGames;
+    private int count;
+
+	void Start ()
+    {
+		foreach(Game g in SaveLoad.savedGames)
+        {
+            
+        }
+	}
+
+
+}
