b2bf5d78cfa4930f143a74784d3a67ee1bf6c60e
jvdwijk <unconfigured@null.spigotmc.org>
2017-03-21 22:29:49 +0100

    script name change & menu fix

--- NewAtlantis/Assets/Scripts/Serialization/Load.cs
+++ NewAtlantis/Assets/Scripts/Serialization/Load.cs
@@ -12,36 +12,5 @@ public class Load : MonoBehaviour {
         SaveLoad.Load();
     }
 
-    void OnGUI()
-    {
-        GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
-        GUILayout.BeginHorizontal();
-        GUILayout.FlexibleSpace();
-        GUILayout.BeginVertical();
-        GUILayout.FlexibleSpace();
-
-        print(SaveLoad.savedGames.Count);
-
-        if (uiEnabled)
-        {
-            foreach (Game g in SaveLoad.savedGames)
-            {
-                GUILayout.Box("test");
-                GUILayout.Space(20);
-                if(g != null)
-                {
-                    if (GUILayout.Button(g.ToString()))
-                    {
-                        Game.current = g;
-                    }
-                }
-            }
-        }
-
-        GUILayout.FlexibleSpace();
-        GUILayout.EndVertical();
-        GUILayout.FlexibleSpace();
-        GUILayout.EndHorizontal();
-        GUILayout.EndArea();
-    }
+    
 }
