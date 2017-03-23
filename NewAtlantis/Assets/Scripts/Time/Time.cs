5f1806ab3bf601f37e0ac028bd3d8ea0eace6378
jvdwijk <joeyvdwijk@gmail.com>
2017-03-23 13:58:12 +0100

    time management

---
+++ NewAtlantis/Assets/Scripts/Time/Time.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Time : MonoBehaviour {
+
+    static int minutes = 290;
+    static int hours;
+
+    public void Start()
+    {
+        StartCoroutine(TimeProgress());
+    }
+
+     IEnumerator TimeProgress()
+    {
+        while (hours < 25)
+        {
+            minutes += 10;
+            hours = minutes / 60;
+            yield return new WaitForSeconds(10);
+        }  
+    }
+}
