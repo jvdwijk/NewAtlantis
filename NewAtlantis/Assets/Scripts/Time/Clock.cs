6430d091f86be0a8ed6a338a0d49d4dfd09c60bb
jvdwijk <unconfigured@null.spigotmc.org>
2017-03-27 19:26:06 +0200

    clock done, scripts commented

---
+++ NewAtlantis/Assets/Scripts/Time/Clock.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clock : MonoBehaviour {
+
+    [SerializeField]
+    private Text uiText;
+
+	void Update()
+    {
+        if (Time.hours > 24) //if day passed
+        {
+            UpdateClockUI(24);
+        }
+        else //if on same day
+        {
+            UpdateClockUI(0);
+        }
+    }
+
+    private void UpdateClockUI(int lessTime)
+    {
+        //if clock strikes an hour (example: 12:00, 13:00)
+        if (Time.minutes != 0)
+        {
+            uiText.text = (Time.hours - lessTime) + ":" + Time.minutes;
+        }
+        else
+        {
+            uiText.text = (Time.hours - lessTime) + ":0" + Time.minutes;
+        }
+    }
+}
--- NewAtlantis/Assets/Scripts/Time/Time.cs
+++ NewAtlantis/Assets/Scripts/Time/Time.cs
@@ -4,21 +4,38 @@ using UnityEngine;
 
 public class Time : MonoBehaviour {
 
-    static int minutes = 290;
-    static int hours;
+    [Header("Time in hours")] //Define at what hour you want to start and end the day.
+    [SerializeField] 
+    private int startTime;
+    [SerializeField] 
+    private int endTime;
+    [Header("Time in secconds")] //Define how long it takes to update the time of your game.
+    [SerializeField] 
+    private float timeUpdate;
 
-    public void Start()
+    //Make it static so you can use it in your other scripts.
+    static public int hours;
+    static public int minutes;
+    static public int totalMinutes;
+
+    private void Start()
     {
+        totalMinutes = startTime * 60;
         StartCoroutine(TimeProgress());
     }
 
+    /// <summary>
+    /// calculate what your new time is, until the day ends.
+    /// </summary>
      IEnumerator TimeProgress()
     {
-        while (hours < 25)
+        while (hours < endTime)
         {
-            minutes += 10;
-            hours = minutes / 60;
-            yield return new WaitForSeconds(10);
+            totalMinutes += 10;
+            hours = totalMinutes / 60;
+            minutes = totalMinutes - (hours * 60);
+
+            yield return new WaitForSeconds(timeUpdate);
         }  
     }
 }
